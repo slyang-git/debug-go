1b3be445ef54559844d3565911d22cf847141181
Bruce Yang <slyang@aliyun.com>
2023-01-07 00:20:55 +0800

    Update

--- cmd/dlv.go
+++ cmd/dlv.go
@@ -5,21 +5,22 @@
 // 1) supply directly in debug dlv [processID] [listen port], e.g.: debug dlv -i 1234 -p 8017
 // 2) supply by read a config file. (process ID cant't be read as a fix value cause it aways changing during debugging sessions);
 // 3) supply a process ID table, users can select the right one;
-// 
+//
 // When a user only run `debug dlv`, it will read a config file firstly. If there is no config file, it will promote a error msg ask user to supply a process ID.
 
 package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var (
 	dlvCmd = &cobra.Command{
-		Use: "dlv",
-		Short: "hello",
-		Long: `hello world hello world`,
+		Use:   "dlv",
+		Short: "Start a dlv process for debugging",
+		Long:  `hello world hello world`,
 		Run: func(cmd *cobra.Command, args []string) {
 			// do something here
 			fmt.Println(args)
@@ -29,6 +30,5 @@ var (
 
 func init() {
 	rootCmd.AddCommand(dlvCmd)
-
 
 }
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/slyang-git/debug/cmd"
 )
 
